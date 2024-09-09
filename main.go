9431ec92b35015d87c13a9cc21acdb602acc13ab
Gokul9791647413 <samgokul2002@gmail.com>
2024-09-09 10:35:19 +0530

    my first commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, World!")
+}
+
+func main() {
+    // Set the handler for the root URL path
+    http.HandleFunc("/", helloWorld)
+
+    // Start the HTTP server on port 8081
+    fmt.Println("Server starting on port 8081...")
+    err := http.ListenAndServe(":8081", nil)
+    if err != nil {
+        fmt.Println("Error starting the server:", err)
+    }
+}
